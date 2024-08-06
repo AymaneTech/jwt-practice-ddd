800ea901771a94b84c81b118181bdd6e520fabf8
geek <elmainiaymane03@gmail.com>
2024-08-06 12:43:25 +0100

    shit and shit

--- src/main/java/dev/codex/jwt_practice/user/infrastructure/RoleSeeder.java
+++ src/main/java/dev/codex/jwt_practice/user/infrastructure/RoleSeeder.java
@@ -1,16 +1,46 @@
 package dev.codex.jwt_practice.user.infrastructure;
 
+import java.io.InputStream;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import dev.codex.jwt_practice.common.seeder.Seeder;
 import dev.codex.jwt_practice.user.domain.entities.Role;
 import dev.codex.jwt_practice.user.domain.repositories.RoleRepository;
 
 @Component
-public class RoleSeeder extends Seeder<Role, RoleRepository> {
+public class RoleSeeder implements CommandLineRunner {
+
+    private final ObjectMapper objectMapper;
+    private final RoleRepository repository;
+
     public RoleSeeder(RoleRepository repository, ObjectMapper objectMapper) {
-        super(repository, "roles", objectMapper, Role.class);
+        this.repository = repository;
+        this.objectMapper = objectMapper;
     }
+
+    @Override
+    public void run(String... args) {
+        if (repository.count() == 0) {
+            try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/roles.json")) {
+                TypeReference<List<Role>> typeReference = new TypeReference<List<Role>>() {
+                    @Override
+                    public java.lang.reflect.Type getType() {
+                        return objectMapper.getTypeFactory().constructCollectionType(List.class, Role.class);
+                    }
+                };
+                List<Role> entities = objectMapper.readValue(inputStream, typeReference);
+                repository.saveAll(entities);
+            } catch (Exception e) {
+                System.err
+                        .println("Error occurred during seeding database using commandline runner: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
